fefc196c260f0e211ccf1295c80b74dc7bfbd637
lucaslos <lucas@sugestly.com>
2024-08-25 19:19:38 -0300

    add get field props

--- src/main.ts
+++ src/main.ts
@@ -342,41 +342,6 @@ export function useForm<T extends FieldsInitialConfig, M = undefined>({
     }),
   )
 
-  const useFormState = useCallback(
-    ({ mustBeDiffFromInitial = false } = {}) => {
-      type UseFormState = {
-        formFields: FieldsState<T>
-        fieldEntries: [
-          keyof T,
-          FieldState<T[keyof T]['initialValue'], T[keyof T]['metadata']>,
-        ][]
-        formError: string | false
-        someFieldIsLoading: boolean
-        validationWasForced: number
-        isDiffFromInitial: boolean
-        formIsValid: boolean
-      }
-
-      return formStore.useSelector((state): UseFormState => {
-        const fieldEntries = objectTypedEntries(state.fields)
-
-        const { someFieldIsLoading, formIsValid, isDiffFromInitial } =
-          getGenericFormState<T>(state, fieldEntries, mustBeDiffFromInitial)
-
-        return {
-          formFields: state.fields,
-          fieldEntries,
-          formError: state.formError,
-          validationWasForced: state.validationWasForced,
-          someFieldIsLoading,
-          formIsValid,
-          isDiffFromInitial,
-        }
-      })
-    },
-    [formStore],
-  )
-
   type ValueArg<K extends FieldsId> = SetValue<T[K]['initialValue']>
 
   type HandleChangeOptions = {
@@ -483,6 +448,57 @@ export function useForm<T extends FieldsInitialConfig, M = undefined>({
       })
     },
     [formStore, formConfig, performExtraUpdates, tempErrors],
+  )
+
+  const useFormState = useCallback(
+    ({ mustBeDiffFromInitial = false } = {}) => {
+      type UseFormState = {
+        formFields: FieldsState<T>
+        fieldEntries: [
+          keyof T,
+          FieldState<T[keyof T]['initialValue'], T[keyof T]['metadata']>,
+        ][]
+        formError: string | false
+        someFieldIsLoading: boolean
+        validationWasForced: number
+        isDiffFromInitial: boolean
+        formIsValid: boolean
+        getFieldProps: (id: keyof T) => {
+          value: T[keyof T]['initialValue']
+          errors: string[] | null
+          onChange: (value: T[keyof T]['initialValue']) => void
+        }
+      }
+
+      return formStore.useSelector((state): UseFormState => {
+        const fieldEntries = objectTypedEntries(state.fields)
+
+        const { someFieldIsLoading, formIsValid, isDiffFromInitial } =
+          getGenericFormState<T>(state, fieldEntries, mustBeDiffFromInitial)
+
+        return {
+          formFields: state.fields,
+          fieldEntries,
+          formError: state.formError,
+          validationWasForced: state.validationWasForced,
+          someFieldIsLoading,
+          formIsValid,
+          isDiffFromInitial,
+          getFieldProps: (id) => {
+            const fieldState = state.fields[id]
+
+            return {
+              value: fieldState.value,
+              errors: fieldState.errors,
+              onChange: (value) => {
+                handleChange(id, value)
+              },
+            }
+          },
+        }
+      })
+    },
+    [formStore, handleChange],
   )
 
   const forceFormUpdate = useCallback(
